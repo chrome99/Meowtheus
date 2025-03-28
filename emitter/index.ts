7088ee9cd8c5f9ba03f6537da32c6378db3b19c2
Ezra <efeldman207@gmail.com>
2025-03-28 18:48:29 +0300

    [emitter] add cats & random interval

--- emitter/index.ts
+++ emitter/index.ts
@@ -1,27 +1,47 @@
 const MEOW_API_URL = "http://localhost:3000";
-const meowingInterval = 5000;
 
-function sendMeowRequest() {
+const MIN_INTERVAL = 2000;
+const MAX_INTERVAL = 10000;
+
+const cats = ["fluffy", "mittens", "whiskers"];
+
+function getRandomInterval() {
+  return (
+    Math.floor(Math.random() * (MAX_INTERVAL - MIN_INTERVAL + 1)) + MIN_INTERVAL
+  );
+}
+
+function sendMeowRequest(catId: string) {
   return fetch(`${MEOW_API_URL}/meow`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      catId: "fluffy",
+      catId,
       timestamp: new Date().toISOString(),
     }),
   });
 }
 
-function main() {
-  setInterval(async () => {
+function startMeowing(catId: string) {
+  async function meow() {
     try {
-      console.log("Meowing a meow...");
-      const res = await sendMeowRequest();
-      console.log(`[${res.status}] ${res.statusText}`);
+      const res = await sendMeowRequest(catId);
+      console.log(`${catId}: [${res.status}] Successfully meowed`);
     } catch (err: any) {
-      console.log(`Error: ${err.message}`);
+      console.log(`${catId} error: ${err.message}`);
     }
-  }, meowingInterval);
+    scheduleNextMeow();
+  }
+
+  function scheduleNextMeow() {
+    setTimeout(meow, getRandomInterval());
+  }
+
+  scheduleNextMeow();
+}
+
+function main() {
+  cats.forEach(startMeowing);
 }
 
 main();
